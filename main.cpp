781530da5569ca1be5c116d83f97081dbd8fd5b8
Gorgan Rares <rares.gorgan@gmail.com>
2017-06-06 19:58:53 +0300

    Exercitiul 3

--- main.cpp
+++ main.cpp
@@ -2,19 +2,23 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     int a[20], nr, i;
     cout << "Numarul de elemente din sirul a (maximum 20) ";
     cin >> nr;
-    for (i = 0; i < nr; i++) {
+    for (i = 0; i < nr; i++)
+        {
         cout << "a[" << i << "] = ";
         cin >> a[i];
     }
-    for (i = 0; i < nr; i++) {
+    for (i = 0; i < nr; i++)
+    {
         if(a[i] < 0)
             a[i] = -a[i];
     }
-    for (i = 0; i < nr; i++) {
+    for (i = 0; i < nr; i++)
+    {
         cout << a[i] << ", ";
     }
     cout << endl;
